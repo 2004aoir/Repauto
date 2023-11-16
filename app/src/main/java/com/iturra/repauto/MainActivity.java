54734375846be9562a956355fb5b709f0c160acd
Alejandro iturra <alejandro.iturra03@inacapmail.cl>
2023-11-16 10:58:20 -0300

    Main Activity con fragments y boton flotante funcionando, bottom layout de informacion funcionando

--- app/src/main/java/com/iturra/repauto/MainActivity.java
+++ app/src/main/java/com/iturra/repauto/MainActivity.java
@@ -1,37 +1,122 @@
 package com.iturra.repauto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    BottomNavigationView bottomNavigationView;
+
+    FloatingActionButton fab;
 
     Button botonCerrarSesion;
-    private FirebaseAuth mAuth;
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
 
-        /*botonCerrarSesion = findViewById(R.id.botoncerrarsesion);
-
-        botonCerrarSesion.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        fab = (FloatingActionButton)findViewById(R.id.btn_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAuth.signOut();
-                finish();
-                startActivity(new Intent(MainActivity.this,Login.class));
+                showBottomDialog();
+            }
+        });
+        replaceFragment(new TiendaFragment());
+
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.tienda) {
+                replaceFragment(new TiendaFragment());
+            } else if (itemId == R.id.carrito) {
+                replaceFragment(new CarritoFragment());
+            } else if (itemId == R.id.compras) {
+                replaceFragment(new ComprasFragment());
+            } else if (itemId == R.id.usuario) {
+                replaceFragment(new UsuarioFragment());
+            }
+
+            return true;
+        });
+
+
+
+    }
+    private  void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void showBottomDialog() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_info_layout);
+
+        LinearLayout info_info = dialog.findViewById(R.id.info_info);
+        LinearLayout info_ubi = dialog.findViewById(R.id.info_ubi);
+        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+
+        info_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Abriendo informacion de la tienda",Toast.LENGTH_SHORT).show();
 
             }
-        });*/
+        });
+
+        info_ubi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Abriendo la ubicacion de la tienda",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
 
     }
 }
